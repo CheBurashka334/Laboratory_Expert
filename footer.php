cb57af95ab79a87854d32f7d8e8cb7114358f5f4
myaff <grabka74@gmail.com>
2015-09-07 15:00:59 +0500

    сновное

---
+++ footer.php
@@ -0,0 +1,20 @@
+</div> <!-- /.workarea -->
+<div class="footer">
+	<div class="container row">
+		<div class="copy left">
+			© 2015  Laboratory Expert
+		</div>
+		<div class="copy right">
+			Разработчик сайта <a href="http://legacystudio.ru/" target="_blank" class="nostyle">«Legacy»</a>
+		</div>
+	</div>
+</div>
+</div> <!-- /.page -->
+<div class="dark-bg"></div>
+<div class="page-aside">
+	<button class="btn-close close-page-aside"><svg class="icon"><use xlink:href="#cross"/></svg></button>
+	<div class="page-aside-content"></div>
+</div>
+</div> <!-- /.layout -->
+</body>
+</html>
